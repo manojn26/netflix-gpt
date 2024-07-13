1121a54371ba70afd8c9c4f2d18ee5753cedc0d4
Manoj Kumar N <manojcsengineer@gmail.com>
2024-07-13 14:53:10 +0530

    browse-page-video-title-component-developed

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './Header'
 import { useNowPlayingMovies } from '../hooks/useNowPlayingMovies'
+import MainContainer from './MainContainer'
+import SecondaryContainer from './SecondaryContainer'
 
 const Browse = () => {
 
@@ -8,6 +10,20 @@ const Browse = () => {
     return (
         <div>
             <Header />
+            {
+                /*  
+                Maincontainer
+                    - Videocontainer
+                    - Videotitle
+                
+                Secondarycontainer
+                    - Movielist * n
+                        - cards * n
+                
+                */
+            }
+            <MainContainer />
+            <SecondaryContainer />
         </div>
     )
 }
